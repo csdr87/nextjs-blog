9a20a6bb6bd265f57a3b38073db6760514e9d892
csdr87 <cs.dr.87@gmail.com>
2025-05-21 16:48:26 -0600

    feat: better 404 site

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,21 @@
-// pages/404.js
+import Link from 'next/link';
+import styles from '../styles/404.module.css';
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+  return (
+    <div className={styles.container}>
+      <img
+        src="/github-404.svg"
+        alt="GitHub 404 Illustration"
+        width={120}
+        height={120}
+        className={styles.logo}
+      />
+      <h1 className={styles.title}>404</h1>
+      <p className={styles.message}>This is not the web page you are looking for.</p>
+      <Link href="/" className={styles.homeLink}>
+        Go back home
+      </Link>
+    </div>
+  );
 }
